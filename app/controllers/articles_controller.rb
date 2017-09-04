c64768f07cec392e47ce273d055361590f09b035
shashank Demo Project <shashankssharma1294@gmail.com>
2017-09-04 18:36:06 +0530

    crud completed now deploy

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,5 @@
 class ArticlesController < ApplicationController
-
+ before_action :set_article , only: [:edit,:update,:show,:destroy]
 
 	def index
 		@articles = Article.all
@@ -10,7 +10,7 @@ class ArticlesController < ApplicationController
 	end
 
 	def edit
-		@article = Article.find(params[:id])
+		
 	end
 
 	def create
@@ -24,7 +24,7 @@ class ArticlesController < ApplicationController
 	end
 
 	def update
-		@article = Article.find(params[:id])
+		
 		if @article.update(article_params)
 			flash[:notice] = "Article updated succesfully"
 			redirect_to article_path(@article)
@@ -34,20 +34,22 @@ class ArticlesController < ApplicationController
 	end
 
 	def show
-		@article = Article.find(params[:id])
+		
 	end
 
-	# def destroy
-	# 	@article = Article.find(params[:id])
-	# 	if @article.destroy(article_params)
-	# 		flash[:notice] = "Article deleted succesfully"
-	# 		redirect_to article_path(@article)
-	# 	else
-	# 		render 'show'
-	# 	end
-	# end
+	def destroy
+	 	
+		@article.destroy
+		flash[:notice] = "Article deleted succesfully"
+		redirect_to articles_path
+
+	end
 
 	 private
+
+	 def set_article
+	 	@article = Article.find(params[:id])
+	 end
 
 	 def article_params
 	 	params.require(:article).permit(:title , :description)
